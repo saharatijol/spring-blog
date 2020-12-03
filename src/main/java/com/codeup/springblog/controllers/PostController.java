dfe3bf4cfbd47d207a55efc91d8661501df69edf
Sara T <sahara.tijol@gmail.com>
2020-12-03 11:51:25 -0600

    UserRepository inject into PostController

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,19 +2,20 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
 import com.codeup.springblog.repos.PostRepository;
+import com.codeup.springblog.repos.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     private final PostRepository postDao;
+    private final UserRepository userDao;
 
-    public PostController(PostRepository postDao) {
+    public PostController(PostRepository postDao, UserRepository userDao) {
         this.postDao = postDao;
+        this.userDao = userDao;
     }
 
     @GetMapping("/posts")
--- src/main/java/com/codeup/springblog/repos/UserRepository.java
+++ src/main/java/com/codeup/springblog/repos/UserRepository.java
@@ -1,4 +1,8 @@
 package com.codeup.springblog.repos;
 
-public interface UserRepository {
+import com.codeup.springblog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
