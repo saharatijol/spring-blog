68df0ade443c91a814f4cc2b042991ea729efbea
Sara T <sahara.tijol@gmail.com>
2020-12-07 12:05:32 -0600

    Setup user controller

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -1,26 +1,34 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.User;
+import com.codeup.springblog.repos.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class UserController {
+    private UserRepository usersDao;
+    private PasswordEncoder passwordEncoder;
 
+    public UserController(UserRepository usersDao, PasswordEncoder passwordEncoder) {
+        this.usersDao = usersDao;
+        this.passwordEncoder = passwordEncoder;
+    }
     @GetMapping("/signup")
-    public String showSignUpForm() {
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
         return "users/signup";
     }
 
     @PostMapping("/signup")
-    public String signUp(
-            @RequestParam(name = "username") String username,
-            @RequestParam(name = "email") String email,
-            @RequestParam(name = "password") String password
-    ) {
-        User user = new User(username, email, password);
-        User dbUser = userDao.save(user);
-        return "redirect:/posts";
+    public String signUp(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        User dbUser = usersDao.save(user);
+        return "redirect:/login";
     }
 
     // LOGIN - GET
