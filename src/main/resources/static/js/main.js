95d578d8ceec83a5c5c107c36e8f93fc6cabb81c
Sara T <sahara.tijol@gmail.com>
2020-12-07 19:02:49 -0600

    Clean up view all

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,8 @@
 new WOW().init();
+
+let logoutBtn = document.getElementById('logout')
+
+logoutBtn.addEventListener('click', function() {
+    document.getElementById('logoutForm').submit();
+    return false;
+})
