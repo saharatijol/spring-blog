1a18705f25be508a56e748122ac76cd0e05e85e9
Sara T <sahara.tijol@gmail.com>
2020-12-08 22:43:58 -0600

    Skip login page if user is authenticated

--- src/main/java/com/codeup/springblog/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/springblog/controllers/AuthenticationController.java
@@ -1,5 +1,11 @@
 package com.codeup.springblog.controllers;
 
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +14,14 @@ public class AuthenticationController {
 
     @GetMapping("/login")
     public String showLoginForm() {
-        return "users/login";
+        // Skip login page if user is authenticated
+        Authentication token = SecurityContextHolder.getContext().getAuthentication();
+
+        // User NOT logged in
+        if (token instanceof AnonymousAuthenticationToken) {
+            return "users/login";
+
+            // redirect to /posts if logged in
+        } return String.format("redirect:%s", "/posts");
     }
 }
